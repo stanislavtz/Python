c38753009ef9d329c8229f0a2e4b49f0ee7b6919
Stanislav <stanislavtz@gmail.com>
2021-03-22 23:34:13 +0200

    split_by_word update

--- Fundamentals/Exercises/02. Python-Lists/split_by_word.py
+++ Fundamentals/Exercises/02. Python-Lists/split_by_word.py
@@ -1,19 +1,22 @@
-import re
-
 data = input()
-words = re.findall(r"\w+#", data)
+unwanted = ", ; : . ! ( ) \" \' / [ ] \\".split(' ')
+
+for u in unwanted:
+    data = data.replace(u, ' ')
+
+words = list(map(lambda x: x.strip(), data.split()))
+
 lower_case = []
 mixed_case = []
 upper_case = []
 
 for word in words:
-    if re.match(r"[a-z]+", word):
+    if word.islower() and word.isalpha():
         lower_case.append(word)
-    elif re.match(r"[A-Z]+[^a-z]+", word) and word != 'C#':
+    elif word.isupper() and word.isalpha():
         upper_case.append(word)
     else:
         mixed_case.append(word)
-    
 
 print(f"Lower-case: {', '.join(lower_case)}")
 print(f"Mixed-case: {', '.join(mixed_case)}")
