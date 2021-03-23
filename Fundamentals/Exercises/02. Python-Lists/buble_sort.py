006991aaeb25de04c730c095868f4b93e0c5c9b2
Tzekov <stanislavtz@gmail.com>
2021-03-23 11:39:21 +0200

    buble_sort solution

--- Fundamentals/Exercises/02. Python-Lists/buble_sort.py
+++ Fundamentals/Exercises/02. Python-Lists/buble_sort.py
@@ -1,1 +1,9 @@
 nums = list(map(int, input().split()))
+i = -1
+while i < len(nums)-2:
+    i += 1
+    if nums[i] > nums[i+1]:
+        nums[i], nums[i+1] = nums[i+1], nums[i]
+        i = -1
+        
+print(' '.join(map(str,nums)))
