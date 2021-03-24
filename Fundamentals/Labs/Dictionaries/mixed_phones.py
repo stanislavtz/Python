a5bd4e51361c76624122cc82f55de99e870371e3
Tzekov <stanislavtz@gmail.com>
2021-03-24 12:28:30 +0200

    mixed phones solution

---
+++ Fundamentals/Labs/Dictionaries/mixed_phones.py
@@ -0,0 +1,15 @@
+phone_book = {}
+
+entry = input()
+while entry != 'Over':
+    li = list(map(lambda x: x.strip(), entry.split(':')))
+    if li[0].isdigit():
+        phone_book[li[1]] = li[0]
+    else:
+        phone_book[li[0]] = li[1]
+
+    entry = input()
+
+sorted_book = sorted(phone_book.items(), key=lambda x: x[0])
+for k,v in sorted_book:
+    print(f"{k} -> {v}")
