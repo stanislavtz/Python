9f702353181c60d44095bf13ba5bb02528b4ac1c
Tzekov <stanislavtz@gmail.com>
2021-03-26 14:14:08 +0200

    shellbound solution

---
+++ Fundamentals/Labs/Dictionaries/shellbound.py
@@ -0,0 +1,21 @@
+import math
+
+regions = {}
+
+data = input()
+while data != 'Aggregate':
+    region = data.split()[0]
+    size = int(data.split()[1])
+    
+    if not region in regions:
+        regions[region] = []
+
+    if not size in regions[region]:
+        regions[region].append(size)
+
+    data = input()
+
+for k, v in regions.items():
+    avrg =  sum(v) - (sum(v) / len(v))
+
+    print(f"{k} -> {', '.join(map(lambda x: str(x), v))} ({math.ceil(avrg)})")
