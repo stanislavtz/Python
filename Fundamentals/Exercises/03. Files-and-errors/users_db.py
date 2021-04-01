b0d40bb38d11160232912466b94eb3e5e118d892
Stanislav <stanislavtz@gmail.com>
2021-04-01 14:19:05 +0300

    created very primitive users DB using json

--- Fundamentals/Exercises/03. Files-and-errors/users_db.py
+++ Fundamentals/Exercises/03. Files-and-errors/users_db.py
@@ -2,68 +2,77 @@ import json
 
 path = './user_database/users.txt'
 
+def get_user(username, users_list):
+    u_l = list(filter(lambda u: u['username'] == username, users_list))
+
+    if len(u_l) == 0:
+        return None
+
+    return u_l[0]
+
 def register_user(user):
     with open(path, 'a') as u_db:
         u_db.write(json.dumps(user)+'\n')
 
-def login_user(users):
+
+def save_users_status(users):
     with open(path, 'w') as u_db:
-        [u_db.write(user) for user in users]
+        [u_db.write(json.dumps(user)+'\n') for user in users]
+
 
 data = input()
 while data != 'exit':
     command = data.split()[0]
-    
+    username = data.split()[1]
+
     with open(path, 'r') as u_db:
         users = u_db.readlines()
- 
+
+    users_list = list(map(lambda u: json.loads(u), users))
+
+    user = get_user(username, users_list)
+
     if command == 'register':
-        username = data.split()[1]
         password = data.split()[2]
         re_password = data.split()[3]
-        user = {}
-        
-        mapped = map(lambda u: json.loads(u)['username'], users)
-        
-        if username in mapped:
+
+        if user:
             print('The given username already exists.')
             data = input()
             continue
-    
+
         if password != re_password:
             print('The two passwords must match.')
             data = input()
             continue
-        
-        user['username'] = username
-        user['password'] = password
-        user['logged_in'] = False
 
-        register_user(user)
+        new_user = {}
+        new_user['username'] = username
+        new_user['password'] = password
+        new_user['logged_in'] = False
+
+        register_user(new_user)
     elif command == 'login':
-        username = data.split()[1]
         password = data.split()[2]
 
-        user_list = [user for user in users if json.loads(user)['username'] == username]
-        s_user = user_list[0] if len(user_list) > 0 else None
-        
-        if not s_user:
+        if not user:
             print('There is no user with the given username.')
-        elif json.loads(s_user)['password'] != password:
+        elif user['password'] != password:
             print('The password you entered is incorrect.')
         else:
-            if json.loads(s_user)['logged_in'] == False:
-                index = users.index(s_user)
-                users[index] = s_user.replace('false', 'true')
-
-                login_user(users)
-            else:
-                s_user = [user for user in users if 'true' in user][0]
-                if s_user:
-                    print('There is already a logged in user.')
+            if user['logged_in']:
+                print(f'{user["username"]} is already logged in.')
+                data = input()
+                continue
+            user['logged_in'] = True
+            save_users_status(users_list)
     elif command == 'logout':
-        s_user = [user for user in users if 'true' in user][0]
-        if not s_user:
-            print('There is no currently logged in user.')
+        if not user['logged_in']:
+            print(f'{user["username"]} is not currently logged in.')
+            data = input()
+            continue
+
+        user['logged_in'] = False
+        save_users_status(users_list)
 
     data = input()
