e1a9e434384d9a94ef8eabf4468ce3c1bc71b46a
Stanislav <stanislavtz@gmail.com>
2021-04-02 13:41:23 +0300

    refactored re_directory

--- Fundamentals/Exercises/03. Files-and-errors/re_directory.py
+++ Fundamentals/Exercises/03. Files-and-errors/re_directory.py
@@ -1,29 +1,48 @@
-import os, shutil
+import os
+import shutil
 
-def make_dir(f):
+
+def make_dir_from_file_extention(f):
     ext = f.split('.')[-1]
     dir_name = f"{ext}s"
-    if not dir_name in os.listdir(output_path):
-        os.mkdir(f"{output_path}/{dir_name}")
-    
+    path = output_path
+
+    create_dir(dir_name, path)
+
 def replace_file(f):
     ext = f.split('.')[-1]
     dir_name = f"{ext}s"
 
     src = f"{input_path}/{f}"
     dst = f"{output_path}/{dir_name}/{f}"
-    
+
     shutil.copy(src, dst)
+
+def create_dir(dir_name, path):
+    if not dir_name in os.listdir(path):
+        os.mkdir(f"{path}/{dir_name}")
+
+def rename_file(f, v, n_v):
+    if v in f:
+        src = f"{input_path}/{f}"
+        n_f = f.replace(v, n_v)
+        dst = f"{renamed_path}/{n_f}"
+
+        shutil.copy(src, dst)
 
 
 path = './re_directory'
 input_path = f'{path}/input'
 output_path = f'{path}/output'
-
-if not 'output' in os.listdir(path):
-    os.mkdir(f"{path}/output")
+renamed_path = f'{path}/renamed'
 
 root, dirs, files = next(os.walk(input_path))
 
-[make_dir(file) for file in files]
+create_dir('output', path)
+create_dir('renamed', path)
+[make_dir_from_file_extention(file) for file in files]
 [replace_file(file) for file in files]
+
+value_to_rename = input()
+new_value = input()
+[rename_file(file, value_to_rename, new_value) for file in files]
