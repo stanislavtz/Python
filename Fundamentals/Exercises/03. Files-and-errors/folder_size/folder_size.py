e44fd0d0c5ea14df5700e91a7b9c50c94ff0fa20
Stanislav <stanislavtz@gmail.com>
2021-04-03 17:36:29 +0300

    correct path envolved

--- Fundamentals/Exercises/03. Files-and-errors/folder_size/folder_size.py
+++ Fundamentals/Exercises/03. Files-and-errors/folder_size/folder_size.py
@@ -1,11 +1,10 @@
 import os
 size = 0
 
-root, dirs, files  = next(os.walk('./products', topdown=True))
-    # if len(files) == 0:
-    #     break
+root, dirs, files  = next(os.walk('../products', topdown=True))
+    
 for f in files:
-    size += os.stat('./products/'+f).st_size
+    size += os.stat('../products/'+f).st_size
 
-with open('./folder_size/output.txt', 'w') as output_file:
+with open('./output.txt', 'w') as output_file:
     output_file.write(str(size/1024/1024)+'\n')
