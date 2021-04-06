287f0725b8ee3316e2a907ed350fb66da2d3aeed
Tzekov <stanislavtz@gmail.com>
2021-04-06 16:44:01 +0300

    nilapandrom and pyramidic solutions

---
+++ Fundamentals/Exercises/04. Strings-and-text-processing/nilapdromes.py
@@ -0,0 +1,18 @@
+string = input()
+
+while string != 'end':
+    border_lenght = 0
+    for i in range(0, len(string) // 2):
+        l = string[:i+1]
+        r = string[-1-i:]
+        if l == r:
+            current_border_lenght = len(string[:i+1])
+            if current_border_lenght > border_lenght:
+                border_lenght = current_border_lenght
+
+    border = string[:border_lenght]
+    core = string[border_lenght:-border_lenght]
+    if border:
+        print(f"{core}{border}{core}")
+    
+    string = input()
