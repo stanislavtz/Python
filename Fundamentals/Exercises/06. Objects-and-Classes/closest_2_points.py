85f8bf321582709baa4eb2083e49c4e1ee659336
Tzekov <stanislavtz@gmail.com>
2021-04-09 16:39:42 +0300

    closest two points temporary solution

--- Fundamentals/Exercises/06. Objects-and-Classes/closest_2_points.py
+++ Fundamentals/Exercises/06. Objects-and-Classes/closest_2_points.py
@@ -1,5 +1,12 @@
 # from point import Point
-import math
+from sys import maxsize
+from math import sqrt
+
+def calculate_distance(p1, p2):
+    x = abs(p1.x - p2.x)
+    y = abs(p1.y - p2.y)
+    return sqrt(x**2 + y**2)
+
 class Point:
     def __init__(self, x, y):
         self.__x = self.set_x(x)
@@ -27,8 +34,12 @@ class Point:
 
 n = int(input())
 points = []
-min_distance 
+min_distance = maxsize
 for _ in range(n):
     data = list(map(float, input().split()))
     points.append(Point(data[0], data[1]))
 
+for i in range(n-1):
+    distance = calculate_distance(points[i], points[i+1])
+    if distance < min_distance:
+        min_distance = distance
